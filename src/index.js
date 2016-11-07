3435a0dd5fe2668a294e84decbceb04f6b5b89b3
anzizhao <anzizhao@126.com>
2016-11-07 11:27:40 +0800

    上传压缩图片类

--- src/index.js
+++ src/index.js
@@ -1,39 +1,183 @@
+// 压缩图片
+function Compressor(opt){
+    opt = opt || {}
+    this.scale = opt.scale || 0.8;
+    this.support = this.isCanvasSupported(); 
+
+}
+// 接口
+Compressor.prototype.parse = function(files, cb){
+    //读取文件
+    var error ;
+    if( ! this.support ) {
+        error = {
+            err: this.errorCode.notSupportCanvas, 
+            msg: 'not support compress'
+        }
+        return cb( error ); 
+    }
+    if( ! files || ! files.length ) {
+        error = {
+            err: this.errorCode.noFiles, 
+            msg: 'no file'
+        }
+        return cb(error); 
+    }
+    if( this.scale === 1) {
+        error = {
+            err: this.errorCode.scaleEqualOne, 
+            msg: 'scale = 1, no need to compress'
+        }
+        cb(error) 
+    }
+    this.cb = cb;
+    this.readFile(files);
+}
+
+Compressor.prototype.readFile = function(files){
+    //读取文件
+    var i; 
+    var len=files.length; 
+    var count=len;
+    var ret = [];
+    var cThis = this;
+    for (i = 0 ,j = files.length ; i<j ; i++){
+        var fReader = new FileReader();
+        fReader.onload = function (e){
+            var result = fReader.result;
+            var filename = fReader.filename;
+            cThis.compress (result, function(out){
+                out && ret.push({name: filename, data: out, blob:true });
+                count --;
+                if ( count <= 0 ){
+                    cThis.cb && cThis.cb(null, ret) 
+                } 
+            })
+
+        };
+        fReader.filename = files[i].name;
+        fReader.readAsDataURL(files[i]);
+    };
+}
+
+Compressor.prototype.compress = function(image, cb) {
+    if (!image){
+        return cb(false)
+    } 
+    var cThis = this;
+    var canvas = document.createElement('canvas')
+    var img = new Image;
+    img.onload = function(){
+            canvas.width = this.width;
+            canvas.height = this.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+            // TODO 动态获取图片类型
+            var type = 'image/jpeg'
+            var out = canvas.toDataURL(type, cThis.scale);
+            var blob = cThis.dataURItoBlob(out);
+            cb(blob)
+            img = null;
+            canvas = null;
+    };
+    img.src = image;
+}
+Compressor.prototype.errorCode = {
+    notSupportCanvas : 100,
+    noFiles: 101,
+    scaleEqualOne: 102,
+}
+
+Compressor.prototype.isCanvasSupported = function(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+}
+Compressor.prototype.dataURItoBlob =  function( dataURI ){
+    // convert base64 to raw binary data held in a string 
+    var byteString 
+        ,mimestring 
+
+    if(dataURI.split(',')[0].indexOf('base64') !== -1 ) {
+        byteString = atob(dataURI.split(',')[1])
+    } else {
+        byteString = decodeURI(dataURI.split(',')[1])
+    }
+
+    mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    var content = new Array();
+    for (var i = 0; i < byteString.length; i++) {
+        content[i] = byteString.charCodeAt(i)
+    }
+
+    return new Blob([new Uint8Array(content)], {type: mimestring});
+}
+
+function postFile (url, files) {
+    //返回的 blob 对象可以 append 到 FormData对象上 用 ajax 上传  
+    var db = new FormData();
+    files.forEach(function(file){
+        if( file.blob ) {
+            db.append('inputFile', file.data, file.name );
+        } else {
+            db.append('inputFile', file );
+        }
+    })
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.onload = function(){
+        if( this.status !== 200 ) {
+            console.error('upload picture fail, status: ', this.status); 
+        } 
+        console.log('success to upload'); 
+        //var j = JSON.parse(this.body);
+        //console.dir(j);
+    }
+    xhr.send(db);
+}
+
+var g_uploadUrl = 'http://localhost:6330/picture';
 
 var uploader = document.getElementById('uploader');
 uploader.onchange = function(e) {
-
-    var file = this.files[0];
-    var fReader = new FileReader();
-    fReader.onload = function (e){
-        console.log( this.readyState)
-        var result = fReader.result
-
-        var blob = new Blob([event.target.result]); // create blob...
-        window.URL = window.URL || window.webkitURL;
-        var blobURL = window.URL.createObjectURL(blob); // and get it's URL
-
-        // helper Image object
-        //var image = new Image();
-        //image.src = blobURL;
-
-        var tImg = document.getElementById('image');
-        var _canvas = document.getElementById('myCanvas');
-        //var _canvas = document.createElement('canvas')
-        tImg.onload = function(){
-                _canvas.width = this.width;
-                _canvas.height = this.height;
-
-                _canvas.style.width = this.width;
-                _canvas.style.height = this.height;
-                var _context = _canvas.getContext('2d');
-                _context.drawImage(tImg, 0, 0);
-                //document.body.appendChild(_canvas);
-        };
-        //tImg.src = result;
-        tImg.src = blobURL;
-        //image.src = blobURL;
-    };
-    //fReader.readAsDataURL( file );
-    fReader.readAsArrayBuffer(file);
-
+    if( this.files.length === 0) {
+        return  
+    }
+    var mv = this;
+    var compressor = new Compressor({scale: 0.6});
+    compressor.parse(this.files, function(err, outFiles ){
+        if( err )  {
+            console.error( err.msg );
+            // 上传原来的图片 
+            err.err === 
+                compressor.errorCode.notSupportCanvas 
+            switch(err.err) {
+                case  compressor.errorCode.notSupportCanvas:
+                case compressor.errorCode.scaleEqualOne: 
+                    postFile(g_uploadUrl, Array.prototype.slice.call( mv.files )) 
+                    break
+            }
+            return 
+        } 
+        //上传 压缩图片
+        postFile(g_uploadUrl, outFiles ) 
+    });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
